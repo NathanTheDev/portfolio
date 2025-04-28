e5afa47188334e7aaca1c233a152bfa6a0dbb864
Nathan <nathan.smith1922@gmail.com>
2025-04-28 14:13:22 +1000

    Implemented basic threejs back into project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
+import Cube from "./Cube";
+
 const Home = () => {
 	return (
 		<>
-		  <div class="title">
-			<h1>
-			  Hi, I'm <span>Nathan</span>
-			</h1>
-		  </div>
-	
-		  <div class="flex-container">
-			<a href="#" class="flex-button">
-			  About Me
-			</a>
-			<a href="https://github.com/NathanTheDev" target="_blank" class="flex-button">
-			  Projects
-			</a>
-			<a href="mailto:nathan-s-dev@outlook.com" class="flex-button">
-			  Contact Me
-			</a>
+		  <div className="title">
+        <h1>
+          Hi, I'm <span>Nathan</span>
+        </h1>
+      </div>
+
+      <Cube />
+    
+      <div className="flex-container">
+        <a href="#" className="flex-button">
+          About Me
+        </a>
+        <a href="https://github.com/NathanTheDev" target="_blank" className="flex-button">
+          Projects
+        </a>
+        <a href="mailto:nathan-s-dev@outlook.com" className="flex-button">
+          Contact Me
+        </a>
 		  </div>
 		</>
 	  );
