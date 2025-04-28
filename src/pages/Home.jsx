be7c63eae94fad1fce3cffa0665ea0495cad152c
Nathan <nathan.smith1922@gmail.com>
2025-04-28 13:48:35 +1000

    Added pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+const Home = () => {
+	return (
+		<>
+		  <div class="title">
+			<h1>
+			  Hi, I'm <span>Nathan</span>
+			</h1>
+		  </div>
+	
+		  <div class="flex-container">
+			<a href="#" class="flex-button">
+			  About Me
+			</a>
+			<a href="https://github.com/NathanTheDev" target="_blank" class="flex-button">
+			  Projects
+			</a>
+			<a href="mailto:nathan-s-dev@outlook.com" class="flex-button">
+			  Contact Me
+			</a>
+		  </div>
+		</>
+	  );
+};
+
+export default Home;
