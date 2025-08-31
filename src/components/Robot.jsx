2774c4813839d5d0d64676191c3130d35b930fb7
Nathan <nathan.smith1922@gmail.com>
2025-08-31 15:24:39 +1000

    feat: implemented basic scene

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import robotPath from "@/assets/robot.glb";
 
 const RobotModel = () => {
-  const { scene } = useGLTF(robotPath);
+	const { scene } = useGLTF(robotPath);
+	const robotRef = useRef();
+
   scene.traverse((child) => {
-    if (child.isMesh) child.castShadow = true;
+    if (child.isMesh) {
+			child.castShadow = true;
+		}
   });
-  return <primitive object={scene} />;
+
+	scene.position.set(0, 0, 0);
+
+	useFrame(({ clock }) => {
+    if (robotRef.current) {
+      robotRef.current.position.y = 1 + Math.sin(clock.elapsedTime) * 0.5;
+    }
+  });
+
+  return <primitive ref={robotRef} object={scene} />;
 };
 
 const GroundPlane = () => (
-  <mesh rotation-x={-Math.PI / 2} position={[0, 0, 0]} receiveShadow>
-    <planeGeometry args={[50, 50]} />
+  <mesh rotation-x={-Math.PI / 2} position={[0, -0.75, 0]} receiveShadow>
+    <planeGeometry args={[500, 500]} />
     <meshStandardMaterial color="lightgrey" />
   </mesh>
 );
@@ -22,10 +35,10 @@ const Robot = () => {
   return (
     <div className="w-screen h-screen">
       <Canvas shadows camera={{ position: [0, 5, 10], fov: 50 }}>
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={1} />
         <directionalLight
           position={[5, 10, 5]}
-          intensity={1}
+          intensity={10}
           castShadow
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
